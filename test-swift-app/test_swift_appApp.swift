8177cf30c4fcabe583f05b27aa59bb60b7d4e0bb
luis_cruz2 <lcruz3@apple.com>
2023-03-21 10:58:52 -0300

    Initial Commit

---
+++ test-swift-app/test_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_swift_appApp.swift
+//  test-swift-app
+//
+//  Created by Luís Cruz on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct test_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
